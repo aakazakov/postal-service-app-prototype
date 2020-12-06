e9fdfac9a9b4cef40694dd5c3bb4d08f0591c98a
aakazakov <aakazakovv@yandex.ru>
2020-12-06 14:59:11 +0300

    init project

---
+++ app/src/module-info.java
@@ -0,0 +1,3 @@
+module dev.fun.app {
+	requires org.junit.jupiter.api;
+}
---
+++ app/test/dev/fun/app/AppTest.java
@@ -0,0 +1,14 @@
+package dev.fun.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
