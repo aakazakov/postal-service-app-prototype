f376267c64429e8d599babce77b802fe83cbecbe
aakazakov <aakazakovv@yandex.ru>
2020-12-09 17:08:25 +0300

    add adapter

---
+++ app/src/dev/fun/app/employee_service/adapters/ManagerToClientAdapter.java
@@ -0,0 +1,26 @@
+package dev.fun.app.employee_service.adapters;
+
+import dev.fun.app.client_service.interfaces.Clientable;
+import dev.fun.app.employee_service.interfaces.Manageable;
+
+/**
+ * Provides a client interface for use by a manager.
+ */
+public class ManagerToClientAdapter implements Clientable {
+	
+	private Manageable manger;
+	
+	public Manageable getManger() {
+		return manger;
+	}
+	
+	public ManagerToClientAdapter(Manageable manger) {
+		this.manger = manger;	
+	}
+
+	@Override
+	public String info() {
+		throw new UnsupportedOperationException("temporarily");
+	}
+	
+}
